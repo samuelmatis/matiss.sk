53293eeb474ae07805851db58359adf52cb0ccf4
Samuel Matis <samuel@matiss.sk>
2013-11-09 22:44:42 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var application_root = __dirname,
+	path = require('path'),
+	express = require('express')
+
+
+// Create server
+var app = express();
+
+// Configure server
+app.configure(function() {
+	app.use( app.router );
+	app.use( express.logger());
+	app.use( express.static( path.join( application_root, 'static' ) ) );
+});
+
+// Start server
+var port = process.env.PORT || 5000;
+app.listen( port, function() {
+	console.log("Website running on port %d in %s mode", port, app.settings.env );
+});
